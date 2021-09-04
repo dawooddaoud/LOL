acaaf8b910a90731f990a7a5ba24c4442d250b4f
Dawood Daoud <davidanna134@gmail.com>
2021-09-04 17:03:26 +0530

    #2

--- apikey.py
+++ apikey.py
@@ -1,5 +1,5 @@
 from riotwatcher import LolWatcher, ApiError
 
-api_key = 'RGAPI*****************'
+api_key = 'RGAPI*****************' # Replace with your API key
 watcher = LolWatcher(api_key)
 my_region = 'euw1'
